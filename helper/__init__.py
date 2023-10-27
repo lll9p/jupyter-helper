8c27e829f998286c79a6a088980d3f15da0b9aac
lll9p <lll9p.china@gmail.com>
2023-10-27 14:22:29 +0800

    build(__init__.py, pyproject.toml): manually specify version in pyproject.toml

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,10 +1,2 @@
 #!/usr/bin/env python3
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("helper")
-except PackageNotFoundError:
-    __version__ = "unknown version"
-
-
 from .helper import Helper
